fa79089e50a02b5a79780e6e9249f7c2af3fde4c
Jesus Delmoral <jdelmoral@students.mchenry.edu>
2020-09-08 19:35:04 -0500

    Initial commit

---
+++ src/ary.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val food = arrayOf<String>("Bun", "Wiener", "Ketchup", "Mustard", "Relish")
+    println("These items are on a HotDog Cart")
+    for (x in food){
+        println(x)
+    }
+
+}
